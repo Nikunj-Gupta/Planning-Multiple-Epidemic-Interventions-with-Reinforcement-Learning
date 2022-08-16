bcd08012e42bc1a436b18d233aac7cc721060874
Mai Le Xuan Anh <alm818@nyu.edu>
2022-08-16 11:30:45 +0400

    Add seeds to SAC

--- sac_kernel.py
+++ sac_kernel.py
@@ -198,7 +198,7 @@ def parse_args(main_args = None):
 #####
 if __name__ == "__main__":
     args = parse_args()
-    seeds = [2]
+    seeds = [0,1,2,3]
     for seed in seeds:
         args.seed = seed
         run_name = f"{args.gym_id}__{args.exp_name}_scale__{args.seed}__{int(time.time())}"
